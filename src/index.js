e43e3d510da4b6d76da0e51bbef21212f0902fe9
Rommel Rico <me@rommelrico.com>
2020-09-25 07:35:32 -0700

    :sparkles: Performance: Demand Code Loading

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const button = document.createElement('button');
 button.innerText = 'Click me';
-button.onclick = () => {
-  console.log("click");
+button.onclick = async () => {
+  System.import('./image_viewer').then(module => {
+    console.log('module: ', module);
+  })
 };
 
 document.body.appendChild(button);
