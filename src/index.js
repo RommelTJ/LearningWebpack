2549e2cbc5e80b93e965f8f24ceaf98f87ce6571
Rommel Rico <me@rommelrico.com>
2020-09-25 07:47:23 -0700

    :sparkles: Performance: Import calls

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const button = document.createElement('button');
 button.innerText = 'Click me';
 button.onclick = () => {
   import('./image_viewer').then(module => {
-    console.log('module: ', module);
+    module.default();
   })
 };
 
