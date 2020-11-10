9b9201d23c9bceeb426000e44cd8a6e60f06c3c8
Rommel Rico <me@rommelrico.com>
2020-11-09 19:55:26 -0800

    :sparkles: Dynamic: Only run webpack middleware in development

--- WebpackProject/server.js
+++ WebpackProject/server.js
@@ -5,10 +5,12 @@ const webpackConfig = require('./webpack.config');
 
 const app = express();
 
-app.use(
-  webpackMiddleware(
-    webpack(webpackConfig)
-  )
-);
+if (process.env.NODE_ENV !== 'production') {
+  app.use(
+    webpackMiddleware(
+      webpack(webpackConfig)
+    )
+  );
+}
 
 app.listen(3050, () => console.log('listening'));
