bc4ad3dcaef3440534b4379b12c1963bba909f42
Rommel Rico <me@rommelrico.com>
2020-11-09 19:56:46 -0800

    :wrench: Dynamic: More dev config

--- WebpackProject/server.js
+++ WebpackProject/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpack = require('webpack');
-const webpackConfig = require('./webpack.config');
 
 const app = express();
 
 if (process.env.NODE_ENV !== 'production') {
+  const webpackMiddleware = require('webpack-dev-middleware');
+  const webpack = require('webpack');
+  const webpackConfig = require('./webpack.config');
   app.use(
     webpackMiddleware(
       webpack(webpackConfig)
